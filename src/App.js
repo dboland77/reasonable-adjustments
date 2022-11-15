a8db55ceb1c519f8c184f7f367f5bb62105858ff
Dave Boland <daveboland@Daves-Mac-mini.local>
2022-11-15 11:20:50 +0000

    changing text to test ci and cd

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Hello pokey this is the start of your reasonable adjustments app idea
         </a>
       </header>
     </div>

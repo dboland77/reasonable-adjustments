4eba1e7ad3dfe63daffc2202b32992e97336e429
Dave Boland <daveboland@Daves-MacBook-Pro.local>
2022-11-23 09:19:18 +0000

    removing aws

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import logo from "./logo.svg";
-import "@aws-amplify/ui-react/styles.css";
-import {
-  withAuthenticator,
-  Button,
-  Heading,
-  Image,
-  View,
-  Card,
-} from "@aws-amplify/ui-react";
 
-function App({ signOut }) {
+
+function App() {
   return (
-    <View className="App">
-      <Card>
-        <Image src={logo} className="App-logo" alt="logo" />
-        <Heading level={1}>We now have Auth!</Heading>
-      </Card>
-      <Button onClick={signOut}>Sign Out</Button>
-    </View>
+    <div className="App">
+        <div src={logo} className="App-logo" alt="logo" />
+    </div>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
